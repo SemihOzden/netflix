f253212bad2ddb88c4d614cc4afc1a0efeb0493a
Semih <semihozden01@gmail.com>
2020-09-29 11:51:26 +0300

    netflix initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FooterContainer } from './containers/footer';
+import {JumbotronContainer} from './containers/jumbotron';
+
+export default function App() {
+  return (
+    <>
+      <JumbotronContainer/>
+      <FooterContainer/>
+    </>
+  );
+}
+
