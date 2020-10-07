8af24f8bf6d4a51f7aebdd16cdc4cb3ca405cf00
Semih <semihozden01@gmail.com>
2020-10-07 11:02:25 +0300

    user redirection and protected route check added

--- src/app.js
+++ src/app.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home ,Browse ,Signin ,Signup } from './pages';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export default function App() {
+  const user= null;
+
   return (
     <Router>
-      <Route exact path="/browse">
-        <Browse/>
-      </Route>
-      <Route exact path="/signin">
+      <IsUserRedirect 
+        user={user}
+        logedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
         <Signin/>
-      </Route>
-      <Route exact path="/signup">
+      </IsUserRedirect>
+      <IsUserRedirect 
+        user={user}
+        logedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+        exact
+      >
         <Signup/>
-      </Route>
-      <Route exact path={ROUTES.HOME}>
+      </IsUserRedirect>
+      <ProtectedRoute
+        user={user}
+        path={ROUTES.BROWSE}
+        exact
+      >
+        <Browse/>
+      </ProtectedRoute>
+      <IsUserRedirect 
+        user={user}
+        logedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}
+        exact
+      >
         <Home/>
-      </Route>
-      
+      </IsUserRedirect>    
     </Router>
   );
 }
